f1cfd555e6f4e81e311a62636514c8fdb3415ef0
Dzides <idzigun@.ru>
2020-11-21 21:51:35 +0300

    added products

--- gulpfile.js
+++ gulpfile.js
@@ -41,7 +41,8 @@ function scripts() {
     return src([
     'node_modules/jquery/dist/jquery.js',
     'node_modules/slick-carousel/slick/slick.js',
-    // 'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
+    'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
+    'node_modules/rateyo/src/jquery.rateyo.js',
     'app/js/main.js'
     ])
     .pipe(concat('main.min.js'))
